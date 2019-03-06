c3e8a998e4ff5a3e28e8a411ca16cc489373baa2
Amangnair <47894206+Amangnair@users.noreply.github.com>
2019-03-06 12:08:52 +0530

    Add files via upload

---
+++ functnpra.py
@@ -0,0 +1,38 @@
+
+def func1():
+	print("hi")
+	print("hello")
+
+func1()	
+
+
+def func2(b):
+	print(b)
+
+func2(3)	
+
+
+
+def func3(a=19)
+	print("age is",a )
+
+func3(15)
+func3()
+
+
+def func4(a,b,c):
+	d=a+b+c
+	print(a,b,c,d)
+
+func4(1,2,3)	
+
+def func5(a,b):
+	c=a+b
+	print(a,b,c)
+	return c  
+
+def func6():
+	d=func5(1,2)
+	print(" new function is:",d)	
+
+func6()	
