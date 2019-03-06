b99da11792c5561fbf0a8e6e41089fb5e691d354
Amangnair <47894206+Amangnair@users.noreply.github.com>
2019-03-06 11:55:18 +0530

    Add files via upload

---
+++ maximum.py
@@ -0,0 +1,13 @@
+
+def func(a,b,c):
+
+	if (a >= b) and (a >= c):
+   		largest = a
+	elif (b >= a) and (b >= c):
+   		largest = b
+	else:
+   		largest = c
+	
+	print("The largest number between",a,",",b,"and",c,"is",largest)
+
+func(1,5,9)
