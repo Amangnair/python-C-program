58800b5ae48ef01fec69df9347c663f0fed3d29c
Amangnair <47894206+Amangnair@users.noreply.github.com>
2019-03-06 12:27:23 +0530

    Add files via upload

---
+++ use_module.py
@@ -0,0 +1,20 @@
+
+from module import add
+
+c=add(4,5)
+print("sum is",c)
+
+
+from module import sub
+c=sub(4,5)
+print("difference is",c)
+
+
+from module import mul
+c=mul(4,5)
+print("product is",c)
+
+
+from module import div
+c=div(4,5)
+print("quotient is",c)
