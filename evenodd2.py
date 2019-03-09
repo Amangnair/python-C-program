593384a0c2404c693f67490310b49e420e95d19e
Amangnair <47894206+Amangnair@users.noreply.github.com>
2019-03-09 11:28:44 +0530

    Add files via upload

---
+++ evenodd2.py
@@ -0,0 +1,12 @@
+
+
+def evnod(a):
+
+	if (a% 2) == 0:
+   		print("{0} is Even".format(a))
+	else:
+   		print("{0} is Odd".format(a))
+
+evnod(3)
+
+evnod(6)
---
+++ listmultiply.py
@@ -0,0 +1,9 @@
+t=[1,2,3,4]
+s=1
+def lmul(t,s):
+	
+	for i in t:
+		s=s*i
+	return s
+
+print(lmul(t,s))
